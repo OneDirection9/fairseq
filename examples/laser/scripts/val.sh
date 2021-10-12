3f7da4763410f66b1f28e6a4d3f389b42988e9fd
Zhipeng Han <hanzhipeng9@gmail.com>
2021-10-12 15:21:57 +0800

    add val.sh
    Signed-off-by: Zhipeng Han <hanzhipeng9@gmail.com>

---
+++ examples/laser/scripts/val.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ -z "${LASER}" ]; then
+  echo "Please set the environment variable 'LASER'"
+  exit
+fi
+
+path="${LASER}/checkpoints/laser_lstm/checkpoint25.pt"
+
+generate() {
+  cfg_file=$1
+  oup_file=$2
+
+  fairseq-generate \
+      "${cfg_file}" \
+      --user-dir laser \
+      --task laser \
+      --gen-subset valid \
+      --path "${path}" \
+      --beam 5 --batch-size 128 --remove-bpe | tee "${oup_file}"
+
+  grep ^T "${oup_file}" | cut -f2- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.ref"
+  grep ^H "${oup_file}" | cut -f3- | perl -ple 's{(\S)-(\S)}{$1 ##AT##-##AT## $2}g' > "${oup_file}.sys"
+}
+
+generate "cfgs/en.json" "output/en.out"
+
+fairseq-score --sys "output/en.out.sys" --ref "output/en.out.ref"
